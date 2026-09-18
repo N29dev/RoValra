d43df4a68f4c74f2a462a25c1f38981f3dfd6f54
Nurlybek <255836@astanait.edu.kz>
2026-09-18 10:39:59 +0500

    Restore index.js and register subplace server list feature

--- src/content/index.js
+++ src/content/index.js
@@ -1,1 +1,3 @@
-PLACEHOLDER
+import { initializeObserver, startObserving } from './core/observer.js';
+import { init as initServerList } from './features/games/serverlist/serverlist.js';
+import { init as initSubplaceServerList } from './features/games/serverlist/SubplaceServerList.js';
